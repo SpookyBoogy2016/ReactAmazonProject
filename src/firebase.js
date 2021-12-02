12874336393c326426ac379a76dc5f3bab577453
Tino Liu <justinoliu.work@gmail.com>
2021-12-02 15:33:16 +0800

    Setup initializeAPP from env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,17 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import "firebase/auth";
 
-const firebaseConfig = {};
+const app = initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+});
 
-const app = initializeApp(firebaseConfig);
+export const auth = app.auth();
 
-export { getAuth, createUserWithEmailAndPassword };
+export default app;
